1d94813a24711253c7722f1729bccc7e3397e502
CarandraugCZ <CarandraugCZ@users.noreply.github.com>
2026-09-17 15:19:41 +0000

    deploy: 3ce8020538048e3c45dfc5097029cbc71bbafcec

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789657633|6238168';
+const CACHE_VERSION = '1789658368|5185713';
 /** @type {string} */
 const CACHE_PREFIX = 'Dark Story-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
