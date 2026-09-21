ecbba710fe33f0a10eb85526effb9ece88b21958
CarandraugCZ <CarandraugCZ@users.noreply.github.com>
2026-09-21 13:58:48 +0000

    deploy: 854130d532e1df7d334f6eb17a8f8b96034a67ac

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789997191|2542219';
+const CACHE_VERSION = '1789999110|2511445';
 /** @type {string} */
 const CACHE_PREFIX = 'Dungeon Recall-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
